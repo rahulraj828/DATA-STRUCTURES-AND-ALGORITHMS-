d7bb121501e6384f168c34aba33d75b4e8e2256d
Rahul raj <rahulraj.work7@gmail.com>
2025-12-16 23:52:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Triplets with Smaller Sum/triplets-with-smaller-sum.py
+++ Difficulty: Medium/Triplets with Smaller Sum/triplets-with-smaller-sum.py
@@ -1,17 +1,19 @@
 class Solution:
     def countTriplets(self, n, sum, arr):
         arr.sort()
-        count = 0
-        for i in range(n-2):
+        n = len(arr)
+        ans = 0
+        for i in range (n-2):
             left = i+1
             right = n-1
-            while left < right :
-                target = arr[i]+arr[left]+arr[right]
-                if target >= sum:
-                    right = right-1
+            
+            while left < right:
+                curr_sum = arr[i]+arr[left]+arr[right]
+                if curr_sum >= sum :
+                    right -=1
                 else:
-                    count = count+(right-left)
-                    left =left+1
+                    ans = ans+right -left
+                    left +=1
+        return ans         
                 
-        return count   
-        
+            
