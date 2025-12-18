36bc19a8b5ba047515fbda2b8d48449778693c37
Rahul raj <rahulraj.work7@gmail.com>
2025-12-18 22:05:23 +0530

    Time: 16 ms (16.86%) | Memory: 27.8 MB (99.95%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -5,11 +5,9 @@ class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         low = 0
         high = 0
-        curr_sum = 0
         res = maxsize
-        n = len(nums)
-
-        while high < n:
+        curr_sum = 0
+        while high < len(nums):
             curr_sum += nums[high]
 
             while curr_sum >= target:
@@ -20,4 +18,11 @@ class Solution:
             high +=1
         return 0 if res ==maxsize else res        
 
-        
+
+
+                
+      
+
+
+
+
