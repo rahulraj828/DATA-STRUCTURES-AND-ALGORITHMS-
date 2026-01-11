b58b186631028c3d33e5f08dbf1b47997d943439
Rahul Raj <rahulraj.work7@gmail.com>
2026-01-11 22:54:30 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (6.52%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,8 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)
         low = 0
-        high = n-1
+        high = len(nums)-1
         while low <= high :
             guess = (low+high)//2
             if nums[guess] == target :
